5db4c56e6ac2009258af577ebb05a0f46e79de1a
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-27 11:29:11 -0400

    Initial Commit

---
+++ Data Practice/Data_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_PracticeApp.swift
+//  Data Practice
+//
+//  Created by Jessica Cooper on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Data_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
