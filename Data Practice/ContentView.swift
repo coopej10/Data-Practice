eaa551a5705a5f3d3e872012a579a94960628d73
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-27 13:39:12 -0400

    Created initial app with a class and an array that stores friends birthdays, and allows for new birthdays to be added.

--- Data Practice/ContentView.swift
+++ Data Practice/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(friendName: "Becca", friendDay: Date(timeIntervalSince1970: 942480000)),
+        Friend(friendName: "Sarah", friendDay: Date(timeIntervalSince1970: 942296400)),
+        Friend(friendName: "Gina", friendDay: Date(timeIntervalSince1970: 821595600))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, style: .date)
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                Button("Save") {
+                    let newFriend = Friend(friendName: newName, friendDay: newBirthday)
+                          friends.append(newFriend)
+                    newName = ""
+                    newBirthday = .now
+                      }
+                      .bold()
+                }
+            }
         }
-        .padding()
+            .padding()
+        }
     }
-}
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
---
+++ Data Practice/Friend.swift
@@ -0,0 +1,20 @@
+//
+//  Friend.swift
+//  Data Practice
+//
+//  Created by Jessica Cooper on 4/27/25.
+//
+
+import Foundation
+
+class Friend {
+    var name = ""
+    var birthday: Date
+   
+    init(friendName: String, friendDay: Date) {
+        name = friendName
+        birthday = friendDay
+    }
+    
+    
+}
